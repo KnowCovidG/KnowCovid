6b9e30f13cdad131aac939487e73278224c81060
KnowCovidG <93266922+KnowCovidG@users.noreply.github.com>
2021-10-27 16:33:50 +0300

    Add files via upload

---
+++ KnowCovid/index.php
@@ -0,0 +1,101 @@
+<html>
+  <head>
+     <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Muli" />
+
+    <link rel="stylesheet" type="text/css" href="stils.css">
+
+    <title>KnowCovid - Grigorijs Smagars</title>
+  </head>
+  <body>
+
+<br>
+
+<div class="container">
+  <img src="https://www.aamc.org/sites/default/files/styles/scale_and_crop_1200_x_666/public/Physician%20Reentry%201200x666.jpg?itok=zIKO_A6Q" alt="Doctors" style="width:80%; opacity:60%;">
+  <div class="centered"><h1 class='logotips'> KnowCovid </h1></div>
+</div>
+<div class='centeredt'> <a class='textlogotips'> Unikāla informācijas vizualizācijas vietne, kas izglīto iedzīvotājus par aktuālākiem COVID-19 izplatības datiem.</a> </div>
+
+
+
+<br>
+  <h1 id='first'> 
+    Jūs jau daudz ko zināt... 
+  </h1> 
+<br> 
+  <div align='center' class='text'> 
+    <p> 
+      COVID-19 situācija mūsu valstī nav apmierināma un visiem Latvijas iedzīvotājiem  kaut-ko jādara. KnowCovid ir vietne, kas palidzēs Jūms vairāk uzzināt par SARS-CoV-2 un iedvesmot palidzēt tiem, kuriem tagad ir visgrūtāk... 
+    </p>
+</div> 
+<div align='center'>
+<div align='center' class='tableauPlaceholder' id='viz1635078811181' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Co&#47;Covid1_16350787400410&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Covid1_16350787400410&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Co&#47;Covid1_16350787400410&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /></object></div></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1635078811181');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else { vizElement.style.width='100%';vizElement.style.height='1427px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+<br> 
+<div class='second' id='second'> <h1> 
+    Vai COVID-19 ir reāli bīstams?  
+  </h1> </div>
+<br> 
+  <div align='center' class='text'> 
+    <p> SARS-CoV-2 ir vīruss ar ~1,53% mirstības koeficentu Latvijā, kad gripam ir tikai ~0,015%. Tikai padomājiet par to, cik daudz cilvēku varētu izglābt... 
+      
+    </p>
+</div> 
+<div align='center'>
+<div class='tableauPlaceholder' id='viz1635078925541' style='position: relative'><noscript><a href='#'><img alt='Dashboard 2 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Co&#47;Covid12&#47;Dashboard2&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Covid12&#47;Dashboard2' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Co&#47;Covid12&#47;Dashboard2&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>  </div>              <script type='text/javascript'>                    var divElement = document.getElementById('viz1635078925541');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else { vizElement.style.width='100%';vizElement.style.height='2727px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+<br> 
+<h1> 
+    Domājat ka vīrusa nav jūsū reģionā?  
+  </h1> 
+<br> 
+  <div align='center' class='text'> 
+    <p> 
+      COVID-19 situācija mūsu valstī nav apmierināma un visiem Latvijas iedzīvotājiem  kaut-ko jādara. KnowCovid ir vietne, kas palidzēs Jūms vairāk uzzināt par SARS-CoV-2 un iedvesmot palidzēt tiem, kuriem tagad ir visgrūtāk... 
+    </p>
+</div> 
+<div align='center'>
+<div class='tableauPlaceholder' id='viz1635098351582' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;co&#47;covidmap_16350983197940&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='covidmap_16350983197940&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;co&#47;covidmap_16350983197940&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div></div>                <script type='text/javascript'>                    var divElement = document.getElementById('viz1635098351582');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else { vizElement.style.width='100%';vizElement.style.height='827px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+<h1> 
+    Kā to var mainīt? 
+  </h1> 
+<br> 
+  <div align='center' class='text'> 
+    <p> 
+      COVID-19 situācija mūsu valstī nav apmierināma un visiem Latvijas iedzīvotājiem  kaut-ko jādara. KnowCovid ir vietne, kas palidzēs Jūms vairāk uzzināt par SARS-CoV-2 un iedvesmot palidzēt tiem, kuriem tagad ir visgrūtāk... 
+    </p>
+</div> 
+
+<div align='center'>
+  <div class='tableauPlaceholder' id='viz1635098684979' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Va&#47;Vakcines&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Vakcines&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Va&#47;Vakcines&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div>  </div>              <script type='text/javascript'>                    var divElement = document.getElementById('viz1635098684979');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else { vizElement.style.width='100%';vizElement.style.height='1677px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement);                </script>
+
+<h1> 
+    Kā to var mainīt? 
+  </h1> 
+<br> 
+  <div align='center' class='text'> 
+    <p> 
+      COVID-19 situācija mūsu valstī nav apmierināma un visiem Latvijas iedzīvotājiem  kaut-ko jādara. KnowCovid ir vietne, kas palidzēs Jūms vairāk uzzināt par SARS-CoV-2 un iedvesmot palidzēt tiem, kuriem tagad ir visgrūtāk... 
+    </p>
+</div> 
+<div align='center'>
+<div class='tableauPlaceholder' id='viz1635145862493' style='position: relative'><noscript><a href='#'><img alt='Dashboard 1 ' src='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Zi&#47;Ziedot&#47;Dashboard1&#47;1_rss.png' style='border: none' /></a></noscript><object class='tableauViz'  style='display:none;'><param name='host_url' value='https%3A%2F%2Fpublic.tableau.com%2F' /> <param name='embed_code_version' value='3' /> <param name='site_root' value='' /><param name='name' value='Ziedot&#47;Dashboard1' /><param name='tabs' value='no' /><param name='toolbar' value='yes' /><param name='static_image' value='https:&#47;&#47;public.tableau.com&#47;static&#47;images&#47;Zi&#47;Ziedot&#47;Dashboard1&#47;1.png' /> <param name='animate_transition' value='yes' /><param name='display_static_image' value='yes' /><param name='display_spinner' value='yes' /><param name='display_overlay' value='yes' /><param name='display_count' value='yes' /><param name='language' value='en-US' /><param name='filter' value='publish=yes' /></object></div> </div>               <script type='text/javascript'>                    var divElement = document.getElementById('viz1635145862493');                    var vizElement = divElement.getElementsByTagName('object')[0];                    if ( divElement.offsetWidth > 800 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else if ( divElement.offsetWidth > 500 ) { vizElement.style.width='1400px';vizElement.style.height='777px';} else { vizElement.style.width='100%';vizElement.style.height='977px';}                     var scriptElement = document.createElement('script');                    scriptElement.src = 'https://public.tableau.com/javascripts/api/viz_v1.js';                    vizElement.parentNode.insertBefore(scriptElement, vizElement); 
+
+             </script>
+<script>
+$("button").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".second").offset().top},
+        'slow');
+});
+    </script>
+<hr> 
+<div class='credits'>
+<a>
+Grigorijs Smagars <br> 
++37126962210 <br> 
+&#169;2021	KnowCovid
+</a>
+</div> 
+
+
+  </body>
+</html>
